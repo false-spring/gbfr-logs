4dc30e8f30c8a83f93c53d39802f0fdd1e1b1126
False Spring <false_spring@protonmail.com>
2024-02-29 17:23:05 -0800

    feat(i18n): Add basic zh-CN support
    Adds character name translations
    
    Closes #3

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,18 +1,33 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import en from "./lang/en.json";
+import zhCN from "./lang/zh-CN.json";
 
 const resources = {
   en,
+  "zh-CN": zhCN,
 };
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: "en",
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+declare global {
+  interface Window {
+    i18n: any;
+  }
+}
+
+window.i18n = i18n;
 
 export default i18n;
