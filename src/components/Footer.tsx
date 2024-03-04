653cfaf6e07971555734120d36c0f1a91ff7bfdc
False Spring <false_spring@protonmail.com>
2024-03-03 16:14:37 -0800

    build: Github Actions for 0.0.4

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -44,7 +44,7 @@ export const Footer = ({ encounterState, elapsedTime }: { encounterState: Encoun
   return (
     <div className="footer transparent-bg font-sm">
       <div className="version">
-        GBFR Logs <span className="version-number">0.0.3</span>
+        GBFR Logs <span className="version-number">0.0.4</span>
       </div>
       {encounterState.totalDamage > 0 && <TeamDamageStats encounterState={encounterState} />}
       <EncounterStatus encounterState={encounterState} elapsedTime={elapsedTime} />
