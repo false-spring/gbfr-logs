4ff2e833e604fb9f0ef676f47d51691dfdac9a76
False Spring <false_spring@protonmail.com>
2024-03-20 22:29:27 -0700

    build: Fix tauri release building

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -6,18 +6,19 @@ use tauri_build::{Attributes, WindowsAttributes};
 fn main() {
     // Build hook library
     println!("cargo:rerun-if-changed=../src-hook/src");
-    let hook_lib_path = fs::canonicalize("../src-hook").unwrap();
-
-    Command::new("cargo")
-        .args(["build", "--release"])
-        .current_dir(&hook_lib_path)
-        .status()
-        .expect("Could not build hook library.");
-
-    // Copy the built library to the tauri app directory
-    let _ = fs::copy("../target/release/hook.dll", "hook.dll");
 
     if cfg!(debug_assertions) {
+        let hook_lib_path = fs::canonicalize("../src-hook").unwrap();
+
+        Command::new("cargo")
+            .args(["build", "--release"])
+            .current_dir(&hook_lib_path)
+            .status()
+            .expect("Could not build hook library.");
+
+        // Copy the built library to the tauri app directory
+        let _ = fs::copy("../target/release/hook.dll", "hook.dll");
+
         tauri_build::build();
     } else {
         let windows = WindowsAttributes::new().app_manifest(include_str!("manifest.xml"));
